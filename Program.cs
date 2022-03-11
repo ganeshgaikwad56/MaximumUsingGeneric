970d2153a52c33b60f51b6853afee0b83f0f37b8
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-11 14:08:01 +0530

    Ganesh Adding Welcome message for Generic program file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaximumGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Greeting Meesage
+            Console.WriteLine("Welcome to Generic Program.");
+
+            //User Input
+            Console.WriteLine("Welcome to the Generics programs");
+            Console.Write("Enter First Number : ");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number : ");
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Number : ");
+            int thirdValue = Convert.ToInt32(Console.ReadLine());
+            //printing Output
+            int resultNum = FindMaximum.ThreeMaxIntNumber(firstValue, secondValue, thirdValue);
+            Console.WriteLine("{0} is maximum value from first: {1} second: {2} third: {3}", resultNum, firstValue, secondValue, thirdValue);
+            Console.ReadLine();
+        }
+    }
+}
