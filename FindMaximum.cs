17ff233f0f4541163b6560d290329864f17d3798
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-11 14:15:38 +0530

    Ganesh Adding program file for UC1

---
+++ FindMaximum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumGeneric
+{
+    public class FindMaximum
+    {
+        /// <summary>
+        /// Threes the maximum int number.
+        /// </summary>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <param name="thirdValue">The third value.</param>
+        /// <returns></returns>
+        public static int ThreeMaxIntNumber(int firstValue, int secondValue, int thirdValue)
+        {
+
+            //PuttingLogic
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+    }
+}
